9933c7951a3d94e85e32704b290649f144835508
Grid23 <hello@grid23.net>
2016-07-11 22:30:20 +0200

    Server class implemented (server-only)

---
+++ server/Server.js
@@ -0,0 +1,114 @@
+"use strict"
+
+const errors = require("./errors")
+const fs = require("fs")
+const http = require("http")
+const https = require("https")
+const klass = require("../lib/class").class
+const objectify = require("../lib/Serializer").Serializer.objectify
+const parse = require("url").parse
+const path = require("path")
+const typeOf = require("../lib/type").typeOf
+
+const Event = require("../lib/Event").Event
+const Route = require("../lib/Route").Route
+const Router = require("../lib/Router").Router
+
+const ROOT_PATH = path.resolve(process.cwd(), path.dirname(require.main && require.main.filename ? require.main.filename : __filename) , "./")
+
+module.exports.Route = klass(Route, statics => {
+    const routes = new WeakMap
+
+    return {
+        constructor: function(request, response){
+            const query = parse(request.url).search||""
+            const fullpath = [request.secure?"https://":"http://",request.headers.host, parse(request.url).pathname, query].join("")
+            const path = ["/", request.method, parse(request.url).pathname].join("")
+
+            Route.call(this, path, { request, response })
+
+            routes.set(this, Object.create(null))
+            routes.get(this).query = objectify(query.slice(1))
+            routes.get(this).fullpath = fullpath
+        }
+      , fullpath: { enumerable: true,
+            get: function(){ return routes.get(this).fullpath }
+        }
+      , query: { enumerable: true,
+            get: function(){ return routes.get(this).query }
+        }
+    }
+})
+
+module.exports.CatchAllRoute = klass(module.exports.Route, statics => {
+    const routes = new WeakMap
+
+    return {
+        constructor: function(request, response){
+            const query = parse(request.url).search||""
+            const fullpath = [request.secure?"https://":"http://",request.headers.host, parse(request.url).pathname, query].join("")
+
+            Route.call(this, "catchall", { request, response })
+            routes.set(this, Object.create(null))
+
+            routes.set(this, Object.create(null))
+            routes.get(this).query = objectify(query.slice(1))
+            routes.get(this).fullpath = fullpath
+        }
+    }
+})
+
+module.exports.Server = klass(Router, statics => {
+    const servers = new WeakMap
+
+    Object.defineProperties(statics, {
+        Route: { enumerable: true,
+            get: function(){ return module.exports.Route
+            }
+        }
+      , CatchAllRoute: { enumerable: true,
+            get: function(){ return module.exports.CatchAllRoute }
+        }
+    })
+
+    return {
+        constructor: function(){
+            Router.apply(this, arguments)
+            servers.set(this, Object.create(null))
+
+            this.Route = module.exports.Route
+        }
+      , CatchAllRoute: { enumerable: true,
+            get: function(){ return servers.get(this).Route || module.exports.CatchAllRoute }
+          , set: function(v){
+                if ( Route.isImplementedBy(v) && typeOf(v) == "function" )
+                  servers.get(this).CatchAllRoute = v
+            }
+        }
+      , listen: { enumerable: true,
+            value: function(){
+                if ( !!servers.get(this).server )
+                  throw new Error(errors.TODO)
+
+                servers.get(this).server = !servers.get(this).secure
+                                         ? new http.Server
+                                         : new https.Server(this.options)
+
+                servers.get(this).server.on("request", (request, response) => {
+                    this.dispatchRoute(new this.Route(request, response))
+                      .addEventListener("routing", e => {
+                          if ( !e.count )
+                            this.dispatchRoute(new this.Route(request, response))
+                              .addEventListener("routing", e => {
+                                  if ( !e.count )
+                                    response.writeHead("404"),
+                                    response.end()
+                              })
+                      })
+                })
+
+                return http.Server.prototype.listen.apply(servers.get(this).server, arguments)
+            }
+        }
+    }
+})
